44c50954ff631ea89ee8f0b500b2d09cb0de82bb
krzyszu1996 <krzyszu1996@users.noreply.github.com>
2020-10-29 14:53:57 +0100

    + zad 8?

---
+++ zad8.py
@@ -0,0 +1,17 @@
+student1 = (102175, "Nisha", "Morrow")
+student2 = (101101, "Tianna", "Mitchell")
+student3 = (118876, "Lorraine", "Cardenas")
+student4 = (103388, "Ariyan", "Tran")
+student5 = (142120, "Judy", "Howarth")
+student6 = (118497, "Savannah", "Hensley")
+student7 = (112620, "Luciano", "Barnett")
+student8 = (131978, "Fahima", "Howard")
+student9 = (128883, "Asiyah", "Dunkley")
+student10 = (144463, "Elisa", "Halliday")
+
+lista_studentow = student1, student2, student3, student4, student5, student6, student7, student8, student9, student10
+
+for i in range(10):
+    print(lista_studentow[i])
+
+#nie jestem pewien czy to o to chodziło :/
