95cbe2d482424a037e2c444e54918e663e5e077f
krzyszu1996 <krzyszu1996@users.noreply.github.com>
2020-10-29 14:11:25 +0100

    + zad 5

---
+++ zad5.py
@@ -0,0 +1,10 @@
+imie = "Krzysztof"
+nazwisko = "Szulc"
+
+eimi = imie[::-1].lower().capitalize()
+oksiwzan = nazwisko[::-1].lower().capitalize()
+
+print(imie)
+print(eimi)
+print(nazwisko)
+print(oksiwzan)
